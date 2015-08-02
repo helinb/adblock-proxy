80d1f10a285343dd07411a1a9072e2b4df7994f3
Christoph Martens <christoph@martens.ms>
2015-08-02 02:32:46 +0200

    The TCP capacitor is down, parse the quantum-state firewall so we can calculate the SSL circuit.

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Test HTTP proxy
-iojs ./bin/proxy --host=localhost --port=8008 --protocol=http;
+iojs ./proxy --public=true;
 
 # Test HTTPS proxy
 # openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem;
