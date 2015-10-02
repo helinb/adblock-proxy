f1445f471f0fb201ba7e403dc25f9a25efa96b64
helinb <1696060289@qq.com>
2015-10-03 03:23:40 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,8 +10,16 @@ wget http://cdn.trjlive.com/hosts/hosts-v8.txt -O ./hosts.d/trjlive.txt
 
 # Update AdBlock Plus Filters
 
-wget --no-check-certificate https://easylist-downloads.adblockplus.org/fanboy-annoyance.txt -O ./adblockplus.d/fanboy-annoyance.txt
-wget --no-check-certificate https://easylist-downloads.adblockplus.org/fanboy-social.txt -O ./adblockplus.d/fanboy-social.txt
-wget --no-check-certificate https://easylist-downloads.adblockplus.org/easylist_noelemhide.txt -O ./adblockplus.d/easylist_noelemhide.txt
-wget --no-check-certificate https://easylist-downloads.adblockplus.org/easyprivacy.txt -O ./adblockplus.d/easyprivacy.txt
+#wget --no-check-certificate https://easylist-downloads.adblockplus.org/fanboy-annoyance.txt -O ./adblockplus.d/fanboy-annoyance.txt
+#wget --no-check-certificate https://easylist-downloads.adblockplus.org/fanboy-social.txt -O ./adblockplus.d/fanboy-social.txt
+#wget --no-check-certificate https://easylist-downloads.adblockplus.org/easylist_noelemhide.txt -O ./adblockplus.d/easylist_noelemhide.txt
+#wget --no-check-certificate https://easylist-downloads.adblockplus.org/easyprivacy.txt -O ./adblockplus.d/easyprivacy.txt
 
+
+wget --no-check-certificate  https://easylist-downloads.adblockplus.org/easylist.txt -O ./adblockplus.d/easylist.txt 
+wget --no-check-certificate	https://easylist-downloads.adblockplus.org/antiadblockfilters.txt -O ./adblockplus.d/antiadblockfilters.txt
+wget --no-check-certificate	https://spam404bl.com/spam404scamlist.txt -O ./adblockplus.d/spam404scamlist.txt
+wget --no-check-certificate	https://raw.githubusercontent.com/nfer/easylistchina_it/master/easylistchina_it.txt -O ./adblockplus.d/easylistchina_it.txt
+wget --no-check-certificate	https://github.com/gythialy/chinalist/raw/master/my_custom_list.txt -O ./adblockplus.d/my_custom_list.txt
+wget --no-check-certificate https://github.com/JinsongVan/chinalist/raw/master/china_mobile_list.txt -O ./adblockplus.d/china_mobile_list.txt
+wget --no-check-certificate https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt -O ./adblockplus.d/easylistchina+easylist.txt 
